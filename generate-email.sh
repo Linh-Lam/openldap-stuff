17a0a12ba11039d2552baa35d38577723e17be78
Linh-Lam <Linh-Lam@users.noreply.github.com>
2015-06-26 00:59:10 +0700

    Update generate-email.sh

--- generate-email.sh
+++ generate-email.sh
@@ -2,7 +2,7 @@
 
 while read line
 do
-result=`ldapsearch -D uid=llam,ou=People,dc=example,dc=com -w <password> -xLLL displayName="$line" | grep mail: | cut -f2 -d" "`
+result=`ldapsearch -D dn=ldap1,cn=example,cn=com -U uid=llam,ou=People,dc=example,dc=com -w <password> -xLLL displayName="$line" | grep mail: | cut -f2 -d" "`
 if [[ -n $result ]];then
   echo "$result"
  else
